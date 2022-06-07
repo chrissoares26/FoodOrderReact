44917ab995a79d59a887865defb988574199d7ff
Chris Soares <76973022+chrissoares26@users.noreply.github.com>
2022-06-07 02:52:03 -0700

    init FoodOrder React project

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from "react";
+
+import mealsImage from "../../assets/meals.jpeg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>Meal App</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of foods" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
